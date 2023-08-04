ac456cb3122f3606d6faef2d5e85ca6c6042e1ff
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-08-04 23:06:57 +0500

    redux added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,21 +3,32 @@ import {copy,linkIcon,loader,tick} from "../assets"
 
 
 const Demo = () => {
+  const [article,setArticle] = useState({
+    url:'',
+    summary:'',
+  })
+
+  
+
+  const handleSubmit = async() =>{
+    
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
 {/* search */}
 <div className="flex flex-col w-full gap-2">
   <form className="relative flex justify-center items-center"
-  onSubmit={()=>{
-
-  }}
+  onSubmit={handleSubmit}
   >
   <img src={linkIcon} alt="link_icon" className="absolute ml-3 left-0 my-2 w-5"/>
 <input type="url"
 placeholder="Enter a URL" 
-  value=""
+  value={article.url}
   required
-  onChange={()=>{
+  onChange={(e)=>{setArticle({
+    ...article, url: e.target.value
+  })
 
   }}
   className="url_input peer"
